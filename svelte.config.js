1319e1390f1ab0e5771170fd86b351d53d817d1f
Clay DeGruchy <clay.degruchy+github@gmail.com>
2024-04-12 18:53:59 +0200

    asd

--- svelte.config.js
+++ svelte.config.js
@@ -4,5 +4,10 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  
+  basepath: '/car-trivia-basic/',
+  build: {
+
+    outDir: '../docs'
+  },
+  outDir: '../docs'
 }
